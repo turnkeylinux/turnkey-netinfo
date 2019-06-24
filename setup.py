c74e74fde834fd442496fae78d2bf07007df7aac
Stefan Davis <stefan@turnkeylinux.org>
2019-06-24 03:38:04 +0000

    Migrated from turnkey-pylib

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='TurnkeyNetinfo',
+    version='1.0',
+    description='Python utility for obtaining network information',
+    author='Alon Swartz',
+    url='https://github.com/turnkeylinux/turnkey-netinfo',
+    packages=['netinfo'])
