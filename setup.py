9c7750e900b132477b94888933061bca1e6c8c85
Jeremy Davis <jeremy@turnkeylinux.org>
2022-05-13 08:38:30 +0000

    Add py.typed so mypy will use it's typing.

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@
 
 from distutils.core import setup
 
-setup(name='turnkey-netinfo',
+setup(
+    name='turnkey-netinfo',
     version='1.0',
     description='Python utility for obtaining network information',
     author='Stefan Davis',
     url='https://github.com/turnkeylinux/turnkey-netinfo',
-    packages=['netinfo'])
+    packages=['netinfo'],
+    package_data={"netinfo": ["py.typed"]})
